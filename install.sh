3679b94e74050b83f2b76441fcdd77a30ade725c
vnmdcvpfug <vnmdcvpfug@gmail.com>
2025-01-29 13:18:00 -0500

    incremental 2

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-echo 70 | sudo tee /sys/class/power_supply/BAT1/charge_start_threshold
-echo 80 | sudo tee /sys/class/power_supply/BAT1/charge_stop_threshold
 sudo cp -r ~/dotfiles/sudoers /etc/sudoers
-sudo cp -r ~/dotfiles/nanorc /etc/nanorc
-sudo cp -r ~/dotfiles/setbrightness /usr/bin/setbrightness
+sudo cp -r ~/dotfiles/brightnessctl /usr/bin/brightnessctl
+sudo cp -r ~/dotfiles/batteryctl.service /etc/systemd/system/batteryctl.service
 cp -r ~/dotfiles/blender ~/blender
 cd
 mkdir opt
@@ -21,9 +19,13 @@ sudo cp -r ./obfs4proxy/obfs4proxy /usr/bin/
 cd ~/dotfiles
 ln -s ~/dotfiles/hypr ~/.config/hypr
 ln -s ~/dotfiles/kitty ~/.config/kitty
-sudo pacman -S chromium hyprland kitty nano noto-fonts noto-fonts-cjk noto-fonts-emoji tor torsocks
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/ranger ~/.config/ranger
+ln -s ~/dotfiles/zathura ~/.config/zathura
+sudo pacman -S chromium hyprland hyprpaper kitty noto-fonts noto-fonts-cjk noto-fonts-emoji nvim python-pillow ranger tor torsocks wl-clipboard xdg-desktop-portal-hyprland zathura zathura-pdf-mupdf
 sudo cp -r ~/dotfiles/torrc /etc/tor/
 sudo systemctl enable --now tor
-rm -v ~/.bashrc
+rm ~/.bashrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+cd
 hyprland
